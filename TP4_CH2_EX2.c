ce2e5a1764f8f64a3f837d742d32afec4e47b5d1
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2022-11-05 13:13:01 +0100

    Create TP4_CH2_EX2.c

---
+++ TP4_CH2_EX2.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <conio.h>
+#include <ctype.h>
+using namespace std;
+
+
+
+int main(int argc, char *argv[])
+{
+	
+	?
+
+
+
+}
