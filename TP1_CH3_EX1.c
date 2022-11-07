268816ada1f220f5f77c778a30eb95be5bb12f0b
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2022-11-07 16:05:21 +0100

    Update TP1_CH3_EX1.c

--- TP1_CH3_EX1.c
+++ TP1_CH3_EX1.c
@@ -7,12 +7,34 @@
 #include <ctype.h>
 #include <math.h>
 
+int puissance(int a, int b);
 
 
-int main(int argc, char *argv[])
+int main()
 {
-  
-	?
+	printf("------ A a la puissance B ------\n\n");
 
+	int a, b;
+	printf("Entrer A : ");
+	scanf("%d", &a);
+	printf("Entrer B : ");
+	scanf("%d", &b);
 
+	printf("%d puissance %d = %d.\n\n", a, b, puissance(a, b));
+
+	return 0;
 }
+
+int puissance(int a, int b)
+{
+	int resultat = 1;
+
+	for (int i = 0; i < b; i++)
+	{
+		resultat *= a;
+
+	}
+
+	return resultat;
+}
+
