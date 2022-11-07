036906290429919cc36bf29fcb4c78b32dcef89c
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2022-11-07 16:04:11 +0100

    Create TP1_CH3_EXO1.c

---
+++ TP1_CH3_EXO1.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int puissance(int a, int b);
+
+
+int main()
+{
+	printf("------ A a la puissance B ------\n\n");
+
+	int a, b;
+	printf("Entrer A : ");
+	scanf("%d", &a);
+	printf("Entrer B : ");
+	scanf("%d", &b);
+
+	printf("%d puissance %d = %d.\n\n", a, b, puissance(a, b));
+
+	return 0;
+}
+
+int puissance(int a, int b)
+{
+	int resultat = 1;
+
+	for (int i = 0; i < b; i++)
+	{
+		resultat *= a;
+
+	}
+
+	return resultat;
+}
