75d771626081393fb65c9dead4d261e7ee06c7ad
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2022-12-08 14:58:40 +0100

    Update TP1_CH5.c

--- TP1_CH5.c
+++ TP1_CH5.c
@@ -34,6 +34,72 @@ void closeFile(FILE* file) {
 
 int main(int argc, char ** argv)
 {
+	
+	
+	////////////////////////////////////////////////////////////////////////
+	//EX 1 SUPPLÉMENTAIRES
+	
+	#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <conio.h>
+#include <math.h>
+#include <iostream>
+
+long getFileSize(FILE* file)
+{
+	long position = ftell(file);
+	fseek(file, 0L, SEEK_END);
+	long result = ftell(file);
+	fseek(file, position, SEEK_SET);
+	return result;
+
+}
+
+
+void closeFile(FILE* file) {
+
+	//On vérifie si le fichier est bien fermé ou non
+	if (fclose(file) == 0)
+	{
+		printf("Fermeture reussie\n");
+	}
+	else
+	{
+		printf("Echec de fermeture du fichier\n");
+	}
+
+}
+
+int main(int argc, char ** argv)
+{
+	FILE* alphabet = fopen("./alphabet.txt", "w");
+
+	if (alphabet == NULL)
+	{
+		printf("L'ouverture du fichier %c %cchou%ce\n", 133, 130, 130);
+		if (alphabet != NULL) fclose(alphabet);
+	}
+	else
+	{
+		//Réussite à l'ouverture du fichier :
+		char c;
+		for (char c = 'a'; c <= 'z'; c++)
+		{
+			putc(c, alphabet);
+		}
+		printf("\n");
+
+		printf("Taille fichier dest2 : %ld octets\n", getFileSize(alphabet));
+
+		closeFile(alphabet);
+
+	}
+
+	return 0;
+}
 	FILE* file = fopen("./source.txt", "r");
 	FILE* dest1File = fopen("./dest1.txt", "r");
 	FILE* destFile = fopen("./dest2.txt", "w");
